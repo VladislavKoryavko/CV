d4989f751f5f8f8354476757f5ce9580da60b105
VladislavKoryavko <vladislav.inco@gmail.com>
2023-04-19 13:24:54 +0300

    Added animation for CV cards

--- src/app/cv-part/cv-part.component.ts
+++ src/app/cv-part/cv-part.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { ITitledFillableContainer } from '../right-panel/right-panel.component';
 
 
@@ -7,10 +7,34 @@ import { ITitledFillableContainer } from '../right-panel/right-panel.component';
   templateUrl: './cv-part.component.html',
   styleUrls: ['./cv-part.component.scss']
 })
-export class cvPartComponent {
+export class CvPartComponent {
+  static cvPartNumber: number = 0;
+  readonly id: string;
+  isVisible = false;
+
+  constructor() {
+    var num = ++CvPartComponent.cvPartNumber;
+    this.id = this.constructor.name + "-" + num;
+
+    if (num === 1) {
+      setTimeout(() => { this.isVisible = true; }, 500);
+    }
+  }
 
   @Input() cvPart: ITitledFillableContainer = {
     title: '',
     cvPartItems: []
   };
+
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+    const windowHeight = window.innerHeight;
+    const bounding = document.getElementById(this.id)?.getBoundingClientRect();
+
+    if (bounding) {
+      if (bounding.top < windowHeight) {
+        setTimeout(() => { this.isVisible = true; }, 500);
+      }
+    }
+  }
 }
